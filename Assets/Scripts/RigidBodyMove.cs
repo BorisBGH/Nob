b10ce77035ad632c3874af85db915d85a99c9183
BorisBGH <bvboris87@gmail.com>
2024-05-07 20:55:05 +0300

    Character and movement

--- Assets/Scripts/RigidBodyMove.cs
+++ Assets/Scripts/RigidBodyMove.cs
@@ -7,11 +7,11 @@ public class RigidBodyMove : MonoBehaviour
     [SerializeField] private Rigidbody _rigidbody;
     [SerializeField] private float _speed = 5f;
     [SerializeField] private Joystick _joystick;
-    [SerializeField] private Vector2 _moveInput;
+    private Vector2 _moveInput;
     [SerializeField] private Animator _animator;
-    
 
-   
+
+
     void Update()
     {
         _moveInput = _joystick.Value;
@@ -20,5 +20,9 @@ public class RigidBodyMove : MonoBehaviour
     private void FixedUpdate()
     {
         _rigidbody.velocity = new Vector3(_moveInput.x, 0f, _moveInput.y);
+        if (_rigidbody.velocity != Vector3.zero)
+        {
+            transform.rotation = Quaternion.LookRotation(_rigidbody.velocity, Vector3.up);
+        }
     }
 }
